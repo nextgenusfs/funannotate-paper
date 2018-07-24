21f45dcd1783e43be0f66bff47e34bc2c83ee60f
Jason Stajich <jasonstajich.phd@gmail.com>
2018-07-24 13:36:50 -0700

    move run into folder

--- funannotate-neurospora.sh
+++ funannotate-neurospora.sh
@@ -1,12 +1,23 @@
 #!/bin/bash
+#SBATCH --ntasks 12 --mem 25G --nodes 1 --out neurospora_predict.%A.log -J funNeuroPred
+
+module load funannotate/git-live
+module load python/2.7.12
 
 #shell script to run funannotate on Neurospora crassa
 CPUS=12
 HOMEDIR=`pwd`
 
+mkdir run_Neurospora38
+pushd run_Neurospora38
+
 #download Neurospora reference from FungiDB
-wget http://fungidb.org/common/downloads/Current_Release/NcrassaOR74A/fasta/data/FungiDB-38_NcrassaOR74A_Genome.fasta
-wget http://fungidb.org/common/downloads/Current_Release/NcrassaOR74A/gff/data/FungiDB-38_NcrassaOR74A.gff
+if [ ! -f FungiDB-38_NcrassaOR74A_Genome.fasta ]; then
+    wget http://fungidb.org/common/downloads/Current_Release/NcrassaOR74A/fasta/data/FungiDB-38_NcrassaOR74A_Genome.fasta
+fi
+if [ ! -f FungiDB-38_NcrassaOR74A.gff ]; then
+    wget http://fungidb.org/common/downloads/Current_Release/NcrassaOR74A/gff/data/FungiDB-38_NcrassaOR74A.gff
+fi
 
 #clean up the genome and annotation from FungiDB
 #the data contains both a rRNA contig and a mitochondrial contig, neither can be annotated
@@ -16,7 +27,7 @@ sed 's/ | .*$//g' FungiDB-38_NcrassaOR74A_Genome.fasta > FungiDB-38_NcrassaOR74A
 python $HOMEDIR/fasta_remove.py FungiDB-38_NcrassaOR74A_Genome_edit.fasta remove.list > Ncrassa.genome.fa
 
 #clean up gff file to remove annotations from mito and rRNA
-grep -E -v 'KC683708|KI440765|KI440766|KI440767|KI440768|KI440769|KI440770|KI440771|KI440772|KI440773|KI440774|KI440775|KI440776|KI440777' FungiDB-38_NcrassaOR74A.gff > Ncrassa.clean.gff3
+grep -E -v '^(KC683708|KI440765|KI440766|KI440767|KI440768|KI440769|KI440770|KI440771|KI440772|KI440773|KI440774|KI440775|KI440776|KI440777)' FungiDB-38_NcrassaOR74A.gff > Ncrassa.clean.gff3
 
 echo -e "Now annotate the genome 4 ways 
 	\t1) funannotate busco mediated training
@@ -26,8 +37,12 @@ echo -e "Now annotate the genome 4 ways
 	-------------------------------------------"
 
 #first run repeatmodeler/repeat masker
-funannotate mask -i Ncrassa.genome.fa --cpus $CPUS -o Ncrassa.masked.fa
+if [ ! -f Ncrassa.masked.fa ]; then
+    funannotate mask -i Ncrassa.genome.fa --cpus $CPUS -o Ncrassa.masked.fa
+fi
 REPEATLIB=$(find . -name "repeatmodeler-library*" | sed 's,./,,g')
+
+#exit
  
 #get UniProt/SwissProt proteins, remove those from Ncrassa as to not bias predictions
 grep 'OS=Neurospora crassa' $FUNANNOTATE_DB/uniprot_sprot.fasta | gsed 's/>//g' > neurospora_crassa.list
@@ -48,10 +63,14 @@ funannotate predict -i Ncrassa.masked.fa \
 
 #now run with RNA-seq data. 
 #Download data from SRA as 9 singled ended stranded Illumina from different growth conditions
+
+# do we want to do strand-specific paired end too at some point? --js
 echo "Now downloading RNA-seq data from NCBI SRA"
 for file in SRR957218 SRR5591013 SRR5591014 SRR5591015 SRR5591016 SRR5591017 SRR5591018 SRR5591019 SRR5591020; do
-	fastq-dump --defline-seq '@$sn[_$rn]/$ri' --split-files $file
-	pigz $file\_1.fastq
+    if [ ! -f $file\_1.fastq.gz ];
+	fastq-dump --gzip --defline-seq '@$sn[_$rn]/$ri' --split-files $file
+#	pigz $file\_1.fastq
+    fi
 done
 
 #data sets from different growth conditions
@@ -64,24 +83,26 @@ funannotate train -i Ncrassa.masked.fa \
 				  --isolate OR74A_rna \
 				  --cpus $CPUS
 
-#now run prediction step using the data generated from training
-#now train with PASA models
+# now run prediction step using the data generated from training
+# now train with PASA models
 funannotate predict -i Ncrassa.masked.fa \
-					-o rna_seq \
-					-s "Neurospora crassa" \
-					--isolate OR74A_pasa \
-					--name PASA_ \
-					--rna_bam rna_seq/training/funannotate_train.coordSorted.bam \
-					--pasa_gff rna_seq/training/funannotate_train.pasa.gff3 \
-					--transcript_evidence rna_seq/training/funannotate_train.trinity-GG.fasta \
-					--transcript_alignments rna_seq/training/funannotate_train.transcripts.gff3 \
-					--protein_alignments ncrassa_uniprot.alignments.gff3 \
-					--cpus $CPUS
+    -o rna_seq \
+    -s "Neurospora crassa" \
+    --isolate OR74A_pasa \
+    --name PASA_ \
+    --rna_bam rna_seq/training/funannotate_train.coordSorted.bam \
+    --pasa_gff rna_seq/training/funannotate_train.pasa.gff3 \
+    --transcript_evidence rna_seq/training/funannotate_train.trinity-GG.fasta \
+    --transcript_alignments rna_seq/training/funannotate_train.transcripts.gff3 \
+    --protein_alignments ncrassa_uniprot.alignments.gff3 \
+    --cpus $CPUS
 
 #recover isoforms and update gene models using PASA/Kallisto
 funannotate update -i rna_seq --cpus $CPUS
 
 #want to compare this to a Maker run
+
+modue load maker
 
 #setup MAKER run
 maker -CTL
@@ -114,8 +135,12 @@ gff3_merge -d maker_rna.maker.output/maker_rna_master_datastore_index.log -s -g 
 maker_map_ids --prefix MBASE_ --justify 6 --suffix -T --iterate 1 maker_rna.gff3 > maker_rna.map.ids
 map_gff_ids maker_rna.map.ids maker_rna.gff3
 
+
 #now run annotation comparison
+
 ln -s $HOMEDIR/rna_seq/update_results/Neurospora_crassa_OR74A_rna.gff3 $HOMEDIR/fun_rna-seq.gff3
 ln -s $HOMEDIR/busco_only/predict_results/Neurospora_crassa_OR74A_busco.gff3 $HOMEDIR/fun_busco.gff3
+
 funannotate util compare -f Ncrassa.masked.fa -r Ncrassa.clean.gff3 -q maker_base.gff3 maker_rna.gff3 fun_busco.gff3 fun_rna-seq.gff3 -o compare -c
 
+popd
